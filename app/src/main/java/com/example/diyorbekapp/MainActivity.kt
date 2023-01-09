9e53bee5ff3b41cf7e2bc8d0f3e93be45a84d983
DiyorbekJDK <diyorbekdavronov07072007@gmail.com>
2023-01-09 12:50:18 +0500

    Banana

---
+++ app/src/main/java/com/example/diyorbekapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.diyorbekapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//import android.widget.Button
+//import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //val mytext = "Мам,поздравляю!"
+       // val mybutton = "Жми ещё раз!"
+       // val mytext2 = "Молодец!"
+       // val mytext3 = "Привет От Диёра!"
+       // val mytext4 = "Пока!"
+       // val mytext5 = "Ну, ладно Пока!"
+
+//        val mytext = "Папа,поздравляю!"
+//        val mybutton = "Жми ещё раз!"
+//        val mytext2 = "Молодец!"
+//        val mytext3 = "Привет От Диёра!"
+//        val mytext4 = "Пока!"
+//        val mytext5 = "Ну, ладно Пока!"
+//
+//
+//        val text: TextView=findViewById(R.id.text1)
+//        val text2: TextView=findViewById(R.id.text2)
+//        val button1: Button=findViewById(R.id.button1)
+//
+//        button1.setOnClickListener {
+//            text.text = mytext
+//            button1.text = mybutton
+//            text2.text = mytext2
+//
+//            button1.setOnClickListener {
+//                text.text = mytext4
+//                button1.text = mytext5
+//                text2.text = mytext3
+//               button1.setOnClickListener {
+//                    throw Exception("Пока!")
+//                }
+//            }
+//        }
+    }
+}
